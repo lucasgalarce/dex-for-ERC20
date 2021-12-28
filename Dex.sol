86b19d3fe678abc42b1331d8cc873cbad3865019
lucasgalarce <galarcelucas@gmail.com>
2021-12-28 19:50:58 -0300

    deposit & withdraw

--- Dex.sol
+++ Dex.sol
@@ -1,4 +1,6 @@
-pragma abicoder v2;
+pragma solidity 0.6.3;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
 
 contract Dex {
     struct Token {
@@ -8,6 +10,7 @@ contract Dex {
 
     mapping(bytes32 => Token) public tokens;
     bytes32[] public tokenList;
+    mapping(address => mapping(bytes32 => uint)) public traderBalances;
     address public admin;
 
     constructor() {
@@ -19,8 +22,24 @@ contract Dex {
         tokenList.push(ticker);
     }
 
+    function deposit(uint amount, bytes32 ticker) tokenExist(ticker) external {
+        IRC20(tokens[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        traderBalances[msg.sender][ticker] += amount;
+    }
+
+    function withdraw(uint amount, bytes32 ticker) tokenExist(ticker) external {
+        require(traderBalances[msg.sender][ticker] >= amount, 'Balance too low');
+        traderBalances[msg.sender][ticker] -= amount;
+        IRC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+
     modifier onlyAdmin() {
         require(msg.sender == admin, 'only admin');
+        _;
+    }
+
+    modifier tokenExist(bytes32 ticker) {
+        require(tokens[ticker].tokenAddress != address(0), 'This token does not exist');
         _;
     }
 }
